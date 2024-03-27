cd917401c7d2706412d58f9cc9e7d0b01ca9cde6
abdo <abdelrahmanali2310@gmail.com>
2024-03-27 04:16:51 +0200

    add custom appbae

--- lib/Features/home/presentation/Views/home_view.dart
+++ lib/Features/home/presentation/Views/home_view.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/Features/home/presentation/Views/widget/custom_app_bar.dart';
+import 'package:bookly_app/assets.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +7,10 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Column(
+      children: [
+        CustomAppBar(),
+      ],
+    );
   }
 }
---
+++ lib/Features/home/presentation/Views/widget/custom_app_bar.dart
@@ -0,0 +1,28 @@
+import 'package:bookly_app/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 24, right: 24, top: 40),
+      child: Row(
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 24,
+          ),
+          Spacer(),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search_rounded,
+                size: 24,
+              ))
+        ],
+      ),
+    );
+  }
+}
