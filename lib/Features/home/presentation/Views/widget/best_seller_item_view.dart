cec528ac0f72718ff7df222934e7b43db08a2a10
abdo <abdelrahmanali2310@gmail.com>
2024-04-01 04:22:21 +0200

    end of ui

--- lib/Features/home/presentation/Views/widget/best_seller_item_view.dart
+++ lib/Features/home/presentation/Views/widget/best_seller_item_view.dart
@@ -29,40 +29,74 @@ class BestSellerListViewItem extends StatelessWidget {
             const SizedBox(
               width: 30,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: Text(
-                    'Harry Potter And the Golet on Fire',
-                    style:
-                        Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                const Text(
-                  'J.K. Rowling',
-                  style: Styles.textStyle14,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      '19.33\$',
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: Text(
+                      'Harry Potter And the Golet on Fire',
                       style: Styles.textStyle20
-                          .copyWith(fontWeight: FontWeight.bold),
-                    )
-                  ],
-                )
-              ],
+                          .copyWith(fontFamily: kGtSectraFine),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  const Text(
+                    'J.K. Rowling',
+                    style: Styles.textStyle14,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '19.33\$',
+                        style: Styles.textStyle20
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      const RateItem(),
+                    ],
+                  )
+                ],
+              ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class RateItem extends StatelessWidget {
+  const RateItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Icon(
+          Icons.star,
+          color: Color(0xffFFDD4F),
+        ),
+        SizedBox(
+          width: 6.3,
+        ),
+        Text(
+          '4.8',
+          style: Styles.textStyle16,
+        ),
+        SizedBox(
+          width: 6,
+        ),
+        Text(
+          '(2654)',
+          style: Styles.textStyle14,
+        )
+      ],
     );
   }
 }
